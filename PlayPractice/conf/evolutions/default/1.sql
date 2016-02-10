7055a3a898408bb85b1902ff6da95544fd0ae289
unisty-d-ichinose <d_ichinose@unisty.co.jp>
2016-02-10 16:37:31 +0900

    設定ファイルの追加

---
+++ PlayPractice/conf/evolutions/default/1.sql
@@ -0,0 +1,22 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table dbaccess_model (
+  id                        integer not null,
+  column_one                varchar(10),
+  constraint pk_dbaccess_model primary key (id))
+;
+
+create sequence dbaccess_model_seq;
+
+
+
+
+# --- !Downs
+
+drop table if exists dbaccess_model cascade;
+
+drop sequence if exists dbaccess_model_seq;
+
